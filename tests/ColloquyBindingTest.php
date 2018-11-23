abd9c5e4147c053d09c522617c490dafd63d1ccc
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-11-24 00:17:42 +0100

    Added ColloquyBindingTest ✍️

---
+++ tests/ColloquyBindingTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+use Colloquy\ColloquyBinding;
+use Colloquy\Drivers\MemoryDriver;
+use Colloquy\IdentifierResolverInterface;
+
+class ColloquyBindingTest extends TestCase
+{
+    public function testBothGettersWorkAsExpected()
+    {
+        $identifierResolver = new class implements IdentifierResolverInterface {
+            public function get($object): string
+            {
+                return 'sample-string-identifier';
+            }
+        };
+        $driver = new MemoryDriver;
+
+        $colloquyBinding = new ColloquyBinding($identifierResolver, $driver);
+
+        $this->assertEquals($identifierResolver, $colloquyBinding->getIdentifierResolver());
+        $this->assertEquals($driver, $colloquyBinding->getDriver());
+    }
+}
